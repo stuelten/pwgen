e63777f2a415dc3dfe177cb87c1234b04d96cf5f
Timo Stülten <8837182+stuelten@users.noreply.github.com>
2025-10-01 11:53:18 +0200

    Convert pwgen to TypeScript

---
+++ pwgen-typescript/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# 1. Install prerequisites
+# 2. Build artifact
+# 3. Move to root dir
+
+# Setup for macOS
+if [[ "Darwin" == "$(uname -s)" ]]; then
+  brew install npm
+elif [[ "Linux" == "$( uname -s )" ]]; then
+  apt install npm
+fi
+
+npm install
+npm run build:bin
+
+cp dist/bin/pwgen-typescript .
