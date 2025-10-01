2d1b0e45eabb9bbaf7f6ad76e7b42ffa5035e187
Timo Stülten <8837182+stuelten@users.noreply.github.com>
2025-10-01 12:10:28 +0200

    Add short build script to build for all languages

---
+++ build-all.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# Call build scripts for all languages
+
+# base dir
+BASE_DIR="$( pwd )"
+
+# implementations to call
+EXEC_LANGS="cpp go rust java typescript"
+
+for el in ${EXEC_LANGS}
+do
+  echo "# Build ${el}" >&2
+  EXEC_DIR="${BASE_DIR}/pwgen-$el"
+  cd "$EXEC_DIR" || exit
+
+  ./build.sh
+
+  cd "${BASE_DIR}" || exit
+done
+
+# echo results
+echo
+echo "Results:"
+ls -l pwgen-*/pwgen-*
