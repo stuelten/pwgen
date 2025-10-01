31e6a24dfbf1230704aeecd544f68277a9dc576e
Timo Stülten <8837182+stuelten@users.noreply.github.com>
2025-10-01 12:05:54 +0200

    Add short build script for each language

--- pwgen-rust/build.sh
+++ pwgen-rust/build.sh
@@ -3,11 +3,12 @@
 # 1. Install prerequisites
 # 2. Build artifact
 
-# Setup for macOS
-if [[ "Darwin" == "$(uname -s)" ]]; then
-  curl https://sh.rustup.rs -sSf | sh
-elif [[ "Linux" == "$( uname -s )" ]]; then
-  curl https://sh.rustup.rs -sSf | sh
+# Install rust if not present
+if [[ ! -r "$HOME/.rustup/settings.toml" ]]; then
+  if [[ "Darwin" == "$(uname -s)" || "Linux" == "$(uname -s)" ]]; then
+    curl https://sh.rustup.rs -sSf | sh
+  fi
 fi
 
+# Build
 cargo build --release
